b0d7626a0e0a47c449288fb54a8f2bf8fb927b51
jonesjebaraj <49519158+jonesjebaraj@users.noreply.github.com>
2022-01-26 20:01:32 +0530

    Update DeployCosmosDB.ps1

--- setup/LabDeployment/deploy/Scripts/DeployCosmosDB.ps1
+++ setup/LabDeployment/deploy/Scripts/DeployCosmosDB.ps1
@@ -1,19 +1,19 @@
 param (
-    [string]$databaseName,
-    [string]$covid19BaseUri,
-    [string]$databaseBackupName,
-    [string]$sqlUserName,
-    [string]$sqlPassword,
-    [string]$cosmosDBConnectionString,
-    [string]$cosmosDBDatabaseName
+    [string]$databaseName = "covid19",
+    [string]$covid19BaseUri = "https://github.com/jonesjebarajgit/AzureAnalyticsOH/tree/main/setup/LabDeployment",
+    [string]$databaseBackupName = "covid19.bak",
+    [string]$sqlUserName = "sqladmin",
+    [string]$sqlPassword = "Password.1!!",
+    [string]$cosmosDBConnectionString = "AccountEndpoint=https://covid19-cosmosdb-k6xqmi3m5rdi4.documents.azure.com:443/;AccountKey=gx7aFPXQstfRS8K3GarO5vrWz4BlSHQNVdO8upGyKIAaiyxMmoUberZFwgyj8xKCfDtpq0tXcUl9lyKEJgUNOw==;",
+    [string]$cosmosDBDatabaseName = "covid19"
 )
 
 $dataFolder = "data/"
 $covidFileName = "covid_policy_tracker.csv"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://aka.ms/csdmtool -OutFile dt1.8.3.zip
-Expand-Archive -Path dt1.8.3.zip -DestinationPath .
+Invoke-WebRequest -Uri https://www.microsoft.com/en-us/download/details.aspx?id=46436 -OutFile dt-1.7.zip
+Expand-Archive -Path dt-1.7.zip -DestinationPath .
 $dtutil = Get-ChildItem -Recurse | Where-Object { $_.Name -ieq "Dt.exe" }
 $env:path += ";$($dtutil.DirectoryName)"
 $azcopy = Get-ChildItem -Recurse | Where-Object { $_.Name -ieq "azcopy.exe" }
