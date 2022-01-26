512ea051285ed28f2a7de580b68a80d89dee2ed1
jonesjebaraj <49519158+jonesjebaraj@users.noreply.github.com>
2022-01-26 17:30:46 +0530

    Update SqlVMExtensionDriver.ps1

--- setup/LabDeployment/deploy/Scripts/SqlVMExtensionDriver.ps1
+++ setup/LabDeployment/deploy/Scripts/SqlVMExtensionDriver.ps1
@@ -1,13 +1,13 @@
 param (
-    [string]$ohmdwSqlserverName,    
-    [string]$databaseName,
-    [string]$covid19BaseUri,
-    [string]$databaseDacPacName,
-    [string]$databaseBackupName,
-    [string]$sqlUserName,
-    [string]$sqlPassword,
-    [string]$cosmosDBConnectionString,
-    [string]$cosmosDBDatabaseName
+    [string]$ohmdwSqlserverName = "covid19-sqlserver-k6xqmi3m5rdi4",    
+    [string]$databaseName = "covid19",
+    [string]$covid19BaseUri = "https://github.com/jonesjebarajgit/AzureAnalyticsOH/tree/main/setup/LabDeployment",
+    [string]$databaseDacPacName = "covid19.bacpac",
+    [string]$databaseBackupName = "covid19.bak",
+    [string]$sqlUserName = "sqladmin",
+    [string]$sqlPassword = "Password.1!!",
+    [string]$cosmosDBConnectionString = "AccountEndpoint=https://covid19-cosmosdb-k6xqmi3m5rdi4.documents.azure.com:443/;AccountKey=gx7aFPXQstfRS8K3GarO5vrWz4BlSHQNVdO8upGyKIAaiyxMmoUberZFwgyj8xKCfDtpq0tXcUl9lyKEJgUNOw==;",
+    [string]$cosmosDBDatabaseName = "covid19"
 )
 
 .\DeploySQLVM.ps1 -ohmdwSqlserverName $ohmdwSqlserverName `
